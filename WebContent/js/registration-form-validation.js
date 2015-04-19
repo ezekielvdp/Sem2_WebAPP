3591192fb49c5db908dfd62689f47fa130eec70d
ezekielvdp <ezekielvdp@yahoo.com>
2015-04-19 22:36:53 +0800

    pushing to the last limit

--- WebContent/js/registration-form-validation.js
+++ WebContent/js/registration-form-validation.js
@@ -1,90 +1,112 @@
-function alphanumeric(uadd) {
-	var letters = /^[0-9a-zA-Z-,]+(\s{0,1}[0-9a-zA-Z-, ])*$/;
-	if (uadd.value.match(letters)) {
-        return true;
-	} else {
-		alert('Address must have alphanumeric characters only');
-		uadd.focus();
+function formValidation()
+	{
+		var firstname = document.registration.firstname;
+		var lastname = document.registration.lastname;
+		var address = document.registration.address;
+		var zip = document.registration.zip;
+		var email = document.registration.email;
+		var mobilenum = document.registration.mobilenum;
+
+
+
+		if(allLetter(firstname))
+		{
+
+				if(allLetter(lastname))
+				{
+					if(alphanumeric(address))
+					{
+
+						if(allnumeric(zip))
+						{
+							if(ValidateEmail(email))
+							{
+								if(ValidateMobilenum(mobilenum))
+								{
+									if(validSubmit())
+									{
+									}
+								}
+							}
+						}
+					}
+				}
+
+		}
 		return false;
 	}
-}
 
-function allLetter(name) {
+function allLetter(name)
+{
 	var letters = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/;
-	if (name.value.match(letters)) {
+	if(name.value.match(letters))
+	{
 		return true;
-	} else {
+	}
+	else
+	{
 		alert('Name must have alphabet characters only');
 		name.focus();
 		return false;
 	}
 }
 
-function formValidation() {
-
-    var firstname = document.registration.firstname;
-    var lastname = document.registration.lastname;
-    var uadd = document.registration.address;
-    var uzip = document.registration.zip;
-    var uemail = document.registration.email;
-
-
-    if (allLetter(firstname)) {
-
-        if (allLetter(lastname)) {
-
-				if (alphanumeric(uadd)) {
-
-				    if (allnumeric(uzip)) {
-				        if (ValidateEmail(uemail)) {
-				            if (validSubmit()) {
-
-                                }
-				            }
-				        }
-				    }
-            }
-
-		}
-		return false;
-	}
-
-
-
-
-
-function allnumeric(uzip) {
-	var numbers = /^\d{4}$/;
-	if(uzip.value.match(numbers)) {
-		return true;
-	} else {
-		alert('Please enter a 4-digit ZIP code');
-		uzip.focus();
-		return false;
-	}
-}
-
-function ValidateEmail(uemail) {
-	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	if (uemail.value.match(mailformat)) {
-		    return true;
-	    } else {
-		      alert("You have entered an invalid email address!");
-		      uemail.focus();
-		      return false;
-	    }
-    }
-
-function ValidateMobilenum(mobilenum)
+function alphanumeric(address)
 {
-	    var mailformat = /^\d{11}$/;
-	if(mobilenum.value.match(mailformat))
+	var letters = /^[0-9a-zA-Z-,]+(\s{0,1}[0-9a-zA-Z-, ])*$/;
+	if(address.value.match(letters))
 	{
 		return true;
 	}
 	else
 	{
-		alert("Please input a 11-digit mobile number");
+		alert('Address must have alphanumeric characters only');
+		address.focus();
+		return false;
+	}
+}
+
+
+function allnumeric(zip)
+{
+	var numbers = /^\d{4}$/;
+	if(zip.value.match(numbers))
+	{
+		return true;
+	}
+	else
+	{
+		alert('Please enter a 4-digit ZIP code');
+		zip.focus();
+		return false;
+	}
+}
+
+function ValidateEmail(email)
+{
+	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(email.value.match(mailformat))
+	{
+		return true;
+	}
+	else
+	{
+		alert("You have entered an invalid email address!");
+		email.focus();
+		return false;
+	}
+}
+
+function ValidateMobilenum(mobilenum)
+{
+	var mobileformat = /^\d{11}$/;
+	if(mobilenum.value.match(mobileformat))
+	{
+		return true;
+	}
+	else
+	{
+		alert("Please enter a 11-digit mobile number");
 		mobilenum.focus();
 		return false;
 	}
